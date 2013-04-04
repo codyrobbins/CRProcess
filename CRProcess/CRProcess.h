28650cbe4b203519f237a7f441b5d68e6b235063
Cody Robbins <cody@codyrobbins.com>
2013-04-04 17:14:42 -0400

    Organized the project.

--- CRProcess/CRProcess.h
+++ CRProcess/CRProcess.h
@@ -1,13 +1,3 @@
-//
-//  CRProcess.h
-//  CRProcess
-//
-//  Created by Cody on 04/04/2013.
-//  Copyright (c) 2013 Cody Robbins. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
 @interface CRProcess : NSObject
 
 @end
--- CRProcess/CRProcess.m
+++ CRProcess/CRProcess.m
@@ -1,11 +1,3 @@
-//
-//  CRProcess.m
-//  CRProcess
-//
-//  Created by Cody on 04/04/2013.
-//  Copyright (c) 2013 Cody Robbins. All rights reserved.
-//
-
 #import "CRProcess.h"
 
 @implementation CRProcess
